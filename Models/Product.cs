a77a965b2a48bbde8cd3d44f547ba27172d84e3a
mustafakapucu <mustafakapucu@windowslive.com>
2018-09-10 16:13:29 +0300

    #first commit

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace Fabrika.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double UnitPrice { get; set; }
+        
+    }
+}
